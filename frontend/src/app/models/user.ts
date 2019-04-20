2f7cbaf10718086a1e38399571bcc71144e6835f
tomiglroe <tomiglroe@gmail.com>
2019-04-20 11:59:25 +0200

    Creado modelo de usuario

---
+++ frontend/src/app/models/user.ts
@@ -0,0 +1,11 @@
+export class User {
+
+  constructor(
+    
+    public _id: string,
+    public dogname: string,
+    public email: string,
+    public password: string,
+    public image: string
+  ) { }
+}
