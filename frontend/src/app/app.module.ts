cbabddcdbe127c2cc37336d5218ce3b06dcf78b2
tomiglroe <tomiglroe@gmail.com>
2019-04-20 12:47:10 +0200

    Creado formulario de registro y primeras configuraciones para recuperar esos datos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +18,8 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-register',
@@ -8,15 +10,32 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   public title: string;
+  public user: User;
 
-  constructor() {
+  constructor(
+
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) {
 
     this.title = 'Registra a tu perro';
+    this.user = new User(
+      "",
+      "",
+      "",
+      "",
+      ""
+    )
   }
 
   ngOnInit() {
 
     console.log('Componente de Registro cargado...');
+  }
+
+  onSubmit() {
+
+    console.log(this.user);
     
   }
 
