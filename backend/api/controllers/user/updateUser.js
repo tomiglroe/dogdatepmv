671bccdc79f768564d8eef564812a249a109bd8e
tomiglroe <tomiglroe@gmail.com>
2019-04-22 18:27:48 +0200

    Actualizar datos de usuario terminado

--- backend/api/controllers/user/updateUser.js
+++ backend/api/controllers/user/updateUser.js
@@ -3,26 +3,33 @@
 const User = require('../../models/user');
 
 function updateUser(req, res) {
+  let userId = req.params.id;
+  let update = req.body;
 
-    const userId = req.params.id;
-    const update = req.body;
+  // Borro la password
+  delete update.password;
 
-    //Borro la password
-    delete update.password;
+  if (userId != req.user.sub) {
+    return res.status(500).send({ message: 'No tienes permiso para actualizar los datos del usuario' });
+  }
 
-    if (userId != req.user.sub) {
+  User.find({ email: update.email.toLowerCase() }).exec((err, users) => {
 
-        return res.status(500).send({ message: 'No tienes permiso para actualizar datos de usuario' });
-    }
+    let user_isset = false;
+    users.forEach((user) => {
+      if (user && user._id != userId) user_isset = true;
+    });
+
+    if (user_isset) return res.status(404).send({ message: 'Ya existe ese correo electrónico' });
 
     User.findByIdAndUpdate(userId, update, { new: true }, (err, userUpdated) => {
+      if (err) return res.status(500).send({ message: 'Error en la petición' });
 
-        if (err) return res.status(500).send({ message: 'Error en la petición' });
+      if (!userUpdated) return res.status(404).send({ message: 'No se ha podido actualizar el usuario' });
 
-        if (!userUpdated) return res.status(404).send({ message: 'No se puede actualizar el usuario' });
-
-        return res.status(200).send({ user: userUpdated });
-    })
+      return res.status(200).send({ user: userUpdated });
+    });
+  });
 }
 
 module.exports = updateUser;
